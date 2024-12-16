f67eda6438885c25b251e0bf314441cd6b8ce640
Andrew Vasquez <andrewvasquez0113@gmail.com>
2024-12-15 16:36:27 -0800

    fixed window undefined issue

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,7 +18,7 @@ export default function Index() {
       <Nav />
       <div className="flex-grow">
         <Hero />
-        //
+        <ServiceScroll />
       </div>
       <Footer />
     </div>
